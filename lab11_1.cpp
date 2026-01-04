f0c4def3ebe32ee361b6a524f9f692402d9ee07d
mink803 <inthupa_kinb@cmu.ac.th>
2026-01-04 23:23:19 +0700

    commit add lab11_1.cpp lab11_2.cpp README.md

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int n = 9;
+
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for(int i=0;i<3;i++){
+    cin.get();
+    }
+    
+    srand(time(0));
+    int r = rand() % n;
+
+    cout << "You will get " << grades[r]<<" in this 261102.";
+
+    return 0;
+}
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
